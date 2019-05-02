f8ca821693ee21f736ffb1ee28e3d2b246d4e597
Ivan5 <converse_ivan@hotmail.com>
2019-05-02 12:18:38 -0500

    Take info of tours from a dedicate json data file

--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 import Tour from "../Tour/Tour";
 import "./TourList.scss";
+import { tourData } from "../tourData";
 
 export default class TourList extends Component {
+  state = {
+    tours: tourData
+  };
   render() {
+    const { tours } = this.state;
     return (
       <div>
         <section className="tourlist">
-          <Tour />
+          {tours.map(tour => {
+            return <Tour key={tour.id} tour={tour} />;
+          })}
         </section>
       </div>
     );
